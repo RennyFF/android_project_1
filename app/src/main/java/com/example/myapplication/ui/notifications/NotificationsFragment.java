eb7afc0349e3a410336b638332363236801d480a
root <steam_for_akk@mail.ru>
2024-04-20 16:57:35 +0300

    initial commit

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.appcompat.widget.Toolbar;
@@ -35,17 +34,25 @@ public class NotificationsFragment extends Fragment {
         // Добавляем Toolbar в ActionBar
         AppCompatActivity activity = (AppCompatActivity) getActivity();
         activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         View switchLayoutSound = root.findViewById(R.id.switch_layout_sound);
         View switchLayoutVibration = root.findViewById(R.id.switch_layout_vibration);
+
+        SwitchCompat switchCompat_sound = switchLayoutSound.findViewById(R.id.switch_compat);
+        SwitchCompat switchCompat_vibr = switchLayoutVibration.findViewById(R.id.switch_compat);
+        switchCompat_sound.setText("MeowSound");
+        switchCompat_vibr.setText("MeowVibr");
 
         // Настройте иконки для каждого кастомного макета
         ImageView iconImageViewSound = switchLayoutSound.findViewById(R.id.icon);
         ImageView iconImageViewVibration = switchLayoutVibration.findViewById(R.id.icon);
 
         // Установите иконки
-        iconImageViewSound.setImageResource(R.drawable.ic_wip_sound_24dp);
+        iconImageViewSound.setImageResource(R.drawable.ic_sound_24dp);
         iconImageViewVibration.setImageResource(R.drawable.ic_round_vibration_24dp);
+        iconImageViewSound.setVisibility(View.VISIBLE);
+        iconImageViewVibration.setVisibility(View.VISIBLE);
         return root;
     }
 
