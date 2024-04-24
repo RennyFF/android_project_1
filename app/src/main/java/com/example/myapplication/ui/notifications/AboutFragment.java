47e9bd969d6b31c2310771ea2526caeebf9e03f2
root <steam_for_akk@mail.ru>
2024-04-24 09:46:04 +0300

    initial commit

---
+++ app/src/main/java/com/example/myapplication/ui/notifications/AboutFragment.java
@@ -0,0 +1,37 @@
+package com.example.myapplication.ui.notifications;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.myapplication.R;
+
+public class AboutFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_layout, null); // Используйте новый макет с закругленными углами
+        ImageButton closeButton = dialogView.findViewById(R.id.btn_close);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss(); // Close the dialog when the close button is clicked
+            }
+        });
+
+        builder.setView(dialogView);
+        return builder.create();
+    }
+
+}
--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -5,17 +5,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-
-import androidx.appcompat.widget.SwitchCompat;
-import androidx.appcompat.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
+import com.example.myapplication.ui.notifications.AboutFragment;
+import com.example.myapplication.ui.notifications.NotificationsViewModel;
+import com.example.myapplication.ui.notifications.TermsFragment;
 
 public class NotificationsFragment extends Fragment {
 
@@ -41,8 +49,8 @@ public class NotificationsFragment extends Fragment {
 
         SwitchCompat switchCompat_sound = switchLayoutSound.findViewById(R.id.switch_compat);
         SwitchCompat switchCompat_vibr = switchLayoutVibration.findViewById(R.id.switch_compat);
-        switchCompat_sound.setText("MeowSound");
-        switchCompat_vibr.setText("MeowVibr");
+        switchCompat_sound.setText(R.string.sound);
+        switchCompat_vibr.setText(R.string.vibr);
 
         // Настройте иконки для каждого кастомного макета
         ImageView iconImageViewSound = switchLayoutSound.findViewById(R.id.icon);
@@ -53,6 +61,28 @@ public class NotificationsFragment extends Fragment {
         iconImageViewVibration.setImageResource(R.drawable.ic_round_vibration_24dp);
         iconImageViewSound.setVisibility(View.VISIBLE);
         iconImageViewVibration.setVisibility(View.VISIBLE);
+
+        TextView btn_app_info = root.findViewById(R.id.btn_app_info);
+        btn_app_info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show the dialog fragment
+                AboutFragment dialogFragment = new AboutFragment();
+                dialogFragment.show(getChildFragmentManager(), "AboutFragment");
+            }
+        });
+
+        TextView btn_terms = root.findViewById(R.id.btn_terms_of_service);
+        btn_terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Получение навигационного контроллера
+                NavController navController = Navigation.findNavController(v);
+                // Навигация к фрагменту TermsFragment
+                navController.navigate(R.id.navigation_terms);
+            }
+        });
+
         return root;
     }
 
@@ -61,4 +91,5 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
 }
